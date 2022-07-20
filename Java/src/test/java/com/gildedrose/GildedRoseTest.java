c6909852c8991d64bfb207a239c13d992099d769
semih.cavdar <semih.cavdar@trendyol.com>
2022-07-20 15:40:48 +0300

    mob next [ci-skip] [ci skip] [skip ci]

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +10,16 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        CombinationApprovals.verifyAllCombinations(this::doUpdate,
+            new String[]{"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"},
+            new Integer[]{0,-1},
+            new Integer[]{0,1,10});
     }
 
+    private String doUpdate(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0].toString();
+    }
 }
